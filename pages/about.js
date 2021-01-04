6ece4b2d82a0404fa76d7a0ee22b9c51935f3f2c
Alexander <hello@alexander.works>
2021-01-05 00:19:26 +0200

    Agjust meta tags

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ function About({ pokemon }) {
       <Head>
         <html lang='en' />
         <meta charset='UTF-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='Test website built with Next.js' />
         <title>Pokemon: {pokemon?.name}</title>
       </Head>
       <Header />
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ function Home() {
       <Head>
         <html lang='en' />
         <meta charset='UTF-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='Test website built with Next.js' />
         <title>Home</title>
       </Head>
       <Header />
