69e1888832c3d9bdcb4c1bd4690cf5f5bf44f4a9
Alexander <hello@alexander.works>
2021-01-04 20:21:26 +0200

    Initial desktop commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <h1>Welcome home, Explorer!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint
+        perferendis dolorum accusamus doloremque deserunt nobis placeat error
+        doloribus aperiam sed! Rerum pariatur temporibus quam?
+      </p>
+    </>
+  );
+}
+
+export default Home;
