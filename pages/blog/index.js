935c25267e99549cbdd9810e3ff6f36c1a1f0f64
Alexander <hello@alexander.works>
2021-01-05 02:44:37 +0200

    Add global styles

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -6,7 +6,7 @@ function Blog() {
     <>
       <Head>
         <html lang='en' />
-        <meta charset='UTF-8' />
+        <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta
           name='description'
--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,14 @@ function Home() {
 
   const handleClick = (e) => {
     e.preventDefault();
-    router.push('/about/');
+    router.push('/pokemon/charmander/');
   };
 
   return (
     <>
       <Head>
         <html lang='en' />
-        <meta charset='UTF-8' />
+        <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta
           name='description'
